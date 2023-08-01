cfa62c1c0f1fafa1c000ee3b0f56f30c6cc13d81
Joriza Oliva <Joriza Oliva@LAPTOP-5OPRACTA>
2023-08-01 22:50:43 +0800

    avgs

--- resources/views/ITStaff/Body/header.blade.php
+++ resources/views/ITStaff/Body/header.blade.php
@@ -16,6 +16,9 @@
         <a href="#">
             <span class="material-icons-outlined">notifications</span>
         </a>
+        <a href="{{url('programs_view')}}">
+            <span class="material-icons-outlined">Programs</span>
+        </a>
         <a href="#">
             <span class="material-icons-outlined">login</span>
         </a>
