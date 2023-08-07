27ca550568c3df832a1345926589c79fde8fe06f
Jayferson Begino <128022438+jeffbegino@users.noreply.github.com>
2023-08-07 23:12:33 +0800

    Update_contacts_in_web.php

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/home/contacts', [ProfileController::class, 'contact_view'])->name('profile.contact_view');
+   //Route::get('/home/contacts', [ProfileController::class, 'contact_view'])->name('profile.contact_view');
 });
+
+Route::get('/Contacts/contacts', function () {
+    return view('Contacts.contacts');
+});
+
 Route::get('/Visitor/programs', function () {
     return view('Visitor.programs');
 });
