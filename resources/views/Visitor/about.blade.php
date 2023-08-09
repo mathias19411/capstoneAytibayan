42e7213bfe3eb53a9ee89bc0c9a73069aa363dea
orly20 <orlyencabo8@gmail.com>
2023-08-10 07:23:24 +0800

    Added about page

---
+++ resources/views/Visitor/about.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="{{ asset('about.css') }}">
+		<title>About Page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+	<body style="font-family: 'Poppins', sans-serif">
+
+		<!--First pic ito !-->
+		<div class="slides">
+		<div class="slide slide1">
+			<img id="img" src="images/image1.png">
+		</div>
+		<div class="slide slide2">
+			<img id="img" src="images/image2.png">
+		</div>
+		<div class="slide slide3">
+			<img id="img" src="images/image3.png">
+		</div>
+
+	</div>
+
+		<div class="container">
+			<div class="box">
+			    <h2>Organizational Chart</h2>
+			    <img src="images/orgchart.png">
+			</div>
+
+		 	<div class="main">
+		  		<h2> About </h2>
+				<div class="line"></div>
+				<p>Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaningLorem ipsum has no intelligible meaning</p>
+				<h2> Mandate </h2>
+				<div class="line"></div>
+				<p>Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaningLorem ipsum has no intelligible meaning</p>
+				<h2> Mission </h2>
+				<div class="line"></div>
+				<p>Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaning Lorem ipsum has no intelligible meaningLorem ipsum has no intelligible meaning</p>
+			</div>
+
+			<div class="box">
+			    <h2>History</h2>
+			    <img src="images/history.png"> 
+			    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+		  </div>
+		</div>
+	</body>
+</html>
