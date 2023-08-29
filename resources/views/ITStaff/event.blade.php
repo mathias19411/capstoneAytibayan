0a2857de334bd75c8cb36ca5a03280434ee9c641
orly20 <orlyencabo8@gmail.com>
2023-08-29 13:40:46 +0800

    css

--- resources/views/ITStaff/event.blade.php
+++ resources/views/ITStaff/event.blade.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="{{ asset('Assets/css/itstaff.css') }}">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <script src="https://kit.fontawesome.com/6297197d39.js" crossorigin="anonymous"></script>
-    <script src="{{ asset('itstaff.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 
@@ -206,6 +205,7 @@
                   <script src="https://code.jquery.com/jquery-3.7.0.js"> </script>
                   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"> </script>
                   <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"> </script>
+                  <script src="{{ asset('itstaff.js') }}"></script>
 
     </body>
   </html>
