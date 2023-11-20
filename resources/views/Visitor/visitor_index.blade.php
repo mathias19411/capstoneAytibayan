ea26b4674251fad530d71f9c94e014846e50e091
orly20 <orlyencabo8@gmail.com>
2023-11-21 04:01:42 +0800

    announcement

--- resources/views/Visitor/visitor_index.blade.php
+++ resources/views/Visitor/visitor_index.blade.php
@@ -102,7 +102,7 @@
                     </span>
                         <span class="announcement-time">{{ $timeEvent }}</span>
                 </div>
-                <h5>{{ $announcement->title }}</h5>
+                <h4>{{ $announcement->title }}</h4>
                 <h5>{{ $announcement->message }}</h5>
                 <h6>{{ $announcement->created_at->format('Y-m-d') }}</h6>
             </div>
