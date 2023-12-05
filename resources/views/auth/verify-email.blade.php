04276fdb61ed2de9c028cce2600375af74738f2a
Mathias Bermejo <bermejomathiasjohnoliver@gmail.com>
2023-12-05 13:58:15 +0800

    added email verification

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,4 +1,75 @@
-<x-guest-layout>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Albay Provincial Agricultural Office - Region V</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('Assets/css/authentication.css') }}">
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" />
+
+    {{-- toastr CSS --}}
+    {{-- <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css> --}}
+</head>
+
+<body class="login">
+    <div class="form-container">
+
+        @if(session()->has('message'))
+            <p class="alert alert-info">
+                {{ session()->get('message') }}
+            </p>
+        @endif
+
+        
+
+        <form class="row g-3 login-form" method="POST" action="{{ route('verification.send') }}">
+            @csrf
+            <div class="col-md-4 side-image">
+                <img id="image" src="/images/APAO logo.png">
+            </div>
+            <div class="col-md-8 form">
+                <div class="input-form">
+                    <h3> Email Verification </h3>
+                    <div class="col-md-12 input">
+                        <p class="text-muted">
+                            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+                        </p>
+                    </div>
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        </div>
+                    @endif
+                    
+                    <div class="login-buttons">
+                        <div class="login-button1">
+                        </div>
+                    </div>
+                    <button type="submit" class="button login-button-item">Resend Verification Email</button>
+
+                </div>
+            </div>
+        </form>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="button login-button-item">Logout</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+
+
+</body>
+
+</html>
+
+{{-- <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
@@ -28,4 +99,4 @@
             </button>
         </form>
     </div>
-</x-guest-layout>
+</x-guest-layout> --}}
